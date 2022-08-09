3bd36dbe2f5eb0661110c648b39eca1715619529
igorWebMASTER <igor_1917@hotmail.com>
2022-08-09 00:50:39 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { ThemeProvider } from "styled-components"
+import { defaultTheme } from "./styles/themes/default"
+
+function App() {
+
+  return (
+    <ThemeProvider theme={defaultTheme}>
+
+    </ThemeProvider>
+  )
+}
+
+export default App
