e3cb2b4be1365208b565d2347ea3633bc11b5e1f
igorWebMASTER <igor_1917@hotmail.com>
2022-08-11 22:27:04 -0300

    chore: add lib phosphor-react and transactions table

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { ThemeProvider } from "styled-components"
+import { Transactions } from "./pages/Transactions"
+import { GlobalStyle } from "./styles/global"
 import { defaultTheme } from "./styles/themes/default"
 
 function App() {
 
   return (
     <ThemeProvider theme={defaultTheme}>
-
+      <Transactions />
+      <GlobalStyle />
     </ThemeProvider>
   )
 }
